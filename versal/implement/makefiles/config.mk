b6f0139a1a40303e854ba1b8ac76891ceace06db
Daisuke Sasaki <daisuke@spacecubics.com>
2025-04-29 15:08:00 +0900

    implement: Add script to insert tool version and FPGA part number
    Added a script to embed the tool version and FPGA part number into the
    FPGA implementation environment for Versal devices.
    
    Signed-off-by: Daisuke Sasaki <daisuke@spacecubics.com>

---
+++ versal/implement/makefiles/config.mk
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------------------------
+# Space Cubics Implementation Environment
+# AMD FPGA configuration file
+#
+# Copyright (c) 2025 Space Cubics, LLC.
+# ---------------------------------------------------------------------------
+
+# AMD Vivado Settings
+XILINX_HOME        := /opt/Xilinx
+XILINX_VER         := 2024.2
+export XILINX_VIVADO := $(XILINX_HOME)/Vivado/$(XILINX_VER)
+export PATH := $(XILINX_HOME)/Vivado/$(XILINX_VER)/bin:$(PATH)
+
+# FPGA device param
+DEVICE_PART        := xcve2302-sfva784-1MP-i-S
---
+++ versal/implement/script/set_env.tcl
@@ -0,0 +1,37 @@
+#-----------------------------------------------------------------------------
+# Copyright 2024 Space Cubics, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#-----------------------------------------------------------------------------
+# Set Environment
+#-----------------------------------------------------------------------------
+
+# AMD IP directory
+set ip_dir         ../ip
+
+# RTL directory
+set rtldir         ../rtl
+set veriloglist    ../rtl/rtl.list
+
+# Report directory
+set rptdit         ./report
+
+# HLS directory
+set hlsdir         ../../vitis-hls-vision/
+set hlsmoduledir   sol/impl/verilog
+
+# Device Parameter
+set xil_part       xcve2302-sfva784-1MP-i-S
+
+# Build Machine Setting
+set cpus           8
