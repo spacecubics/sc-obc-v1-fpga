07326d3019a49aee30da0c331c129ee5c0e7f459
Daisuke Sasaki <daisuke@spacecubics.com>
2025-11-05 03:18:04 +0900

    script: Modify script to set VDD for LPD MIO
    Updated the handling of LPD MIO (Bank 502) VDD configuration.
    Unlike PMC MIO (Bank 500/501) and JTAG (Bank 503), the LPD MIO I/O
    voltage can be determined by the user. Therefore, the VDD setting was
    moved from the ip/versal_ps initialization script to a user-level
    script for easier customization.
    
    This configuration will later be made selectable through set_env.tcl.
    
    Signed-off-by: Daisuke Sasaki <daisuke@spacecubics.com>

--- versal/script/implement/create_versal_base_system.tcl
+++ versal/script/implement/create_versal_base_system.tcl
@@ -94,6 +94,8 @@ set_property platform.version {1.0} [current_project]
 set_property pfm_name "spacecubics:${prj_name}:${prj_name}:1.0" [get_files -all ${prj_dir}/${prj_name}.srcs/${file_set}/bd/${bd_name}/${bd_name}.bd]
 set_property platform.uses_pr {false} [current_project]
 
+set_property CONFIG.PS_PMC_CONFIG {PS_BANK_2_IO_STANDARD {LVCMOS1.8}} [get_bd_cells versal_ps]
+
 # Load board-specific setup for OBC Module V1 Evaluation Board
 if {${sc_evb1001} == "yes"} {
     source ${root_dir}/script/implement/sc_evb1001_bd_logic.tcl
